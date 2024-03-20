2060403f1b408bd420b28ef6d45ad527475c20cf
Mohd Sohail Ali Khan <122856021+Sohailkhan55@users.noreply.github.com>
2024-03-20 16:08:02 +0530

    Added Database

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash");
-let posts = [];
+import express from 'express'
+import bodyParser from 'body-parser';
+import ejs from 'ejs'
+import _ from "lodash";
+
+import { dbConnect } from './db.js';
+import { PostModel } from './models/postModel.js';
+
+dbConnect();
+
 
 const homeStartingContent = "At Yours-Blogs, we are passionate about sharing knowledge, inspiring creativity, and fostering a sense of community. Our goal is to provide you with valuable information, insightful stories, and engaging content on a wide range of topics. Whether you're a seasoned expert or just beginning your journey of discovery, our blog is designed to cater to your interests and curiosities.Our team of writers and contributors is dedicated to delivering high-quality, well-researched, and thought-provoking content that informs, entertains, and encourages meaningful conversations. We're here to guide you through the intricacies of various subjects, provide helpful tips and advice, and inspire you to explore new horizons.";
 const aboutContent = "Welcome to Yours-Blogs, your go-to source for a wide array of captivating and informative content. Our blog covers everything from travel adventures, lifestyle, and tech trends to health and wellness tips. Join us on a journey of exploration, knowledge sharing, and inspiration as we bring you engaging articles that cater to your diverse interests and curiosities. Whether you're a seasoned enthusiast or a newcomer to the blogging world, Yours-Blogs has something for everyone.";
@@ -15,56 +20,63 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("public"));//tell express our static files are in public folder
 
-app.get("/",function(req,res){
-  res.render("home" , {startingContent : homeStartingContent ,
-    posts :posts});
+//Create Route
+app.get("/",async function(req,res){
+  try {
+    const posts=await PostModel.find({}).sort({createdAt : -1});
+    res.render("home" , {startingContent : homeStartingContent ,
+      posts :posts});
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
   
 });
-
+//startingContent- variable which we will pass over
 app.get("/about",function(req,res){
   res.render("about",{abtContent:aboutContent});
 });
+
 app.get("/contact",function(req,res){
   res.render("contact",{cntContent:contactContent});
 });
+
+app.post("/",function (req,res) {
+  res.redirect("/compose");
+})
+
 app.get("/compose",function(req,res){
   res.render("compose");
 });
-app.post("/compose",function(req,res){
-  const post = {
+
+app.post("/compose",async function(req,res){
+  const post = new PostModel({
       title: req.body.postTitle,
       content: req.body.postBody
-  };
-  posts.push(post);
+  });
+  await post.save();
   res.redirect("/");
 });
-app.get("/posts/:postName",function(req,res){
-    const requestedTitle = _.lowerCase(req.params.postName);
-    posts.forEach(function(post){
-      const storedTitle = _.lowerCase(post.title);
-      if(storedTitle === requestedTitle)
-      {
-        res.render("post" , {
-          title: post.title,
-          content: post.content
-        });
-      }
-      
-
-    })
+//Express Routing parameters
+//name of parameter can be postName or postid
+app.get("/posts/:postId",async function(req,res){
+  
+    const requestedPostId = req.params.postId;
+    try{
+      const post = await PostModel.findById(requestedPostId);
+      res.render("post",{
+        title : post.title,
+        content : post.content
+      });
+    }
+    catch(err)
+    {
+      console.log(err);
+    }
 });
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
